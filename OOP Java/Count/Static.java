8406558e5ffa70ea56937280768582d6d098023b
Imam Hasan Shawon <152065554+imam21hasan@users.noreply.github.com>
2024-11-29 00:02:37 +0600

    Add files via upload

---
+++ OOP Java/Count/Static.java
@@ -0,0 +1,14 @@
+
+package Count;
+
+public class Static {
+    static int count=0;
+    Static()
+    {
+        count++;
+    }
+    void display()
+    {
+        System.out.println("Number of count : " +count);
+    }
+}
---
+++ OOP Java/Count/Test.java
@@ -0,0 +1,15 @@
+
+package Count;
+
+public class Test {
+    public static void main(String[] args) {
+        Static ob=new Static();
+        ob.display();
+        
+        Static ob1=new Static();
+        ob1.display();
+        
+        Static ob2=new Static();
+        ob2.display();
+    }
+}
