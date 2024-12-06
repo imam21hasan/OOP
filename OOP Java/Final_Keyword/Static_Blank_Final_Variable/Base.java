476ac4f8b8bc3ee281de24910d2c66d5bec61ce5
Imam Hasan Shawon <152065554+imam21hasan@users.noreply.github.com>
2024-12-07 00:09:24 +0600

    Update Base.java

--- OOP Java/Final_Keyword/Static_Blank_Final_Variable/Base.java
+++ OOP Java/Final_Keyword/Static_Blank_Final_Variable/Base.java
@@ -4,7 +4,7 @@ package Final_Keyword.Static_Blank_Final_Variable;
 public class Base {
     static final int n;
     static{
-        n=564121;   // static final variable k static block ar maddhome initialize kora hoy
+        n=564121;
     }
     void show()
     {
