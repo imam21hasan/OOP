f9670a0cb1eb1680d39a7617ca4e6706e6e1ca82
Imam Hasan Shawon <152065554+imam21hasan@users.noreply.github.com>
2024-12-26 11:22:35 +0600

    Add files via upload

---
+++ OOP Java/Pack_2/A.java
@@ -0,0 +1,9 @@
+
+package Pack_2;
+
+public class A {
+    public void show2()
+    {
+        System.out.println("Pack2 A");
+    }
+}
---
+++ OOP Java/Pack_2/B.java
@@ -0,0 +1,12 @@
+
+package Pack_2;
+
+public class B {
+    public static void main(String[] args) {
+         A ob=new A();
+         ob.show2();
+         
+         Pack_1.A ob1=new Pack_1.A();
+         ob1.show();
+    }
+}
